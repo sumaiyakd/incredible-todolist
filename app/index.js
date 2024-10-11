95f7977da8afac662f812a5da3c0b076b40e958b
sumaiyakd <sumaiya.khurshid@edu.sait.ca>
2024-10-11 12:45:02 -0600

    react a

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import App from '../components/App'
+
+const index = () => {
+  return (
+    <View>
+      <App />
+    </View>
+  )
+}
+
+export default index
